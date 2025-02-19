4950c3bc78c64c958883bfef15d02ca60ec8598d
amirhon23 <amirhonahmadov23@gmail.com>
2025-02-19 20:23:18 +0300

    uploading

---
+++ script.js
@@ -0,0 +1,25 @@
+//1
+let num = 0
+
+for (let i = 1; i <= 100; i++) {
+
+    console.log(`${num += i}`);
+
+}
+
+//2
+let names = ["Anirkhon", "Shahram", "Akbar", "Zarruh",]
+
+for (let i = 0; i < names.length; i++) {
+
+   console.log(`Привет, ${names[i]}`)
+
+}
+// 3
+let  string = "text"
+
+for (let i = 0; i < string.length; i++) {
+
+    console.log(string[i]);
+    
+}
